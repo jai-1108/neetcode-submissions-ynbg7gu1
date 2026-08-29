23552134ebafa54a33c72f4312833111b2e3b58e
Jai Vasi <133922578+jai-1108@users.noreply.github.com>
2026-08-29 17:14:52 -0400

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        """
+        position, speed = [(1,3),(4,2)]
+        [(4,2),(1,3)]
+        time = [3,3], time = target-p / s
+        if time and time[-1] >= time[i]:
+            no_of_fleets += 1
+        return no_of_fleets
+        """
+        cars = list(zip(position, speed))
+        cars.sort(reverse = True)
+        stack = []
+        no_of_fleets = 0
+        for p, s in cars:
+            distance = target - p
+            stack.append(distance/s)
+        max_time = 0
+        for t in stack:
+            if t > max_time:
+                no_of_fleets += 1
+                max_time = t
+        return no_of_fleets
+
